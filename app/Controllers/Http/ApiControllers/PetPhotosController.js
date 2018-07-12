af468383bdf786855cf4157fcf1e6ba2d4ba826a
Greg Gushard <greg@flipsidemedia.com>
2018-07-12 14:13:39 -0400

    no message

---
+++ app/Controllers/Http/ApiControllers/PetPhotosController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Pet Photos Controller
+ */
+class PetPhotosController {
+  async storePetPhoto({request, auth, response}) {
+
+  }
+
+  async storePetPhotoCrop({request, auth, response}) {
+
+  }
+}
+
+module.exports = PetPhotosController
